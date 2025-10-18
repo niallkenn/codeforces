54426b3856b229838cca42d4bd06020aaa472cc1
niallkenn <niallkennedy@gmail.com>
2025-10-18 18:47:59 +0100

    2155a accepted

---
+++ 2155a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int teams; cin >> teams;
+    cout << 2 * teams - 2 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int tt; cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
